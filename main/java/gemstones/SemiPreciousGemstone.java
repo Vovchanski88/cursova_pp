ae3005d3b1f6b78f597a42caed0dc1d401152a5a
Vovchanski88 <vovafifa10@gmail.com>
2025-06-11 12:59:09 +0300

    Delete main/java/gemstones/SemiPreciousGemstone.java

--- main/java/gemstones/SemiPreciousGemstone.java
+++
@@ -1,9 +0,0 @@
-package gemstones;
-
-public class SemiPreciousGemstone extends Gemstone {
-    public SemiPreciousGemstone() {}
-
-    public SemiPreciousGemstone(String name, double weight, double price, double transparency) {
-        super(name, weight, price, transparency);
-    }
-}
